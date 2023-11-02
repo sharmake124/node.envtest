1c7fe16b9ebc031e62ec30c57c1e3bccf73c883a
Sharmake Awil <sharmarke.awil@hotmail.com>
2023-11-02 15:04:44 +0100

    env mini projet

---
+++ app.js
@@ -0,0 +1,8 @@
+const dotenv = require("dotenv");
+dotenv.config();
+
+const name = process.env.MY_NAME;
+const city = process.env.MY_CITY;
+const language = process.env.MY_LANGUAGE;
+
+console.log(`I am ${name}, wilder in ${city}, and I love ${language}.`);
